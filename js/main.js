aa27cb917d2e4062daaa36663dce966df076e66a
Yoss-lab <hassaniyosra@gmail.com>
2024-08-04 21:01:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //var hauteurHeader = $('#header').outerHeight();
+  var largeurHeader = $('.header').outerWidth();
+ // $('#main').css('margin-top', hauteurHeader + 'px');
+  $('#main').css('margin-left', largeurHeader + 'px');
+ // $('.sidebar').css('top', hauteurHeader + 'px');
+});
